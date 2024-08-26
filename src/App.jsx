96a7eeb505267d44df505e3786d8e55f721333d5
Kava <126253915+DanFromUkraine@users.noreply.github.com>
2024-08-26 20:22:08 +0300

    Переробив на віддалену картинку

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 
 import './App.css'
 import PetCard from './components/pet_card';
-import cat_img from "./assets/Bella.jpg"
 
 function App() {
 
   return (
     <>
-     <PetCard img={cat_img} name={"Bella"}/>
+     <PetCard img="https://i.ibb.co/V3mW9W1/Bella.jpg" name={"Bella"}/>
     </>
   )
 }
